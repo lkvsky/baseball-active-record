ee942ad994ea96da7adda51ad1b4ebc3e1b812f3
Kyle + Sarah <kyle+sarah@appacademy.io>
2013-01-29 16:31:00 -0800

    removed line that broke 'debugged' manager-team association. finally, actually, debugged

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,4 @@ class Team < ActiveRecord::Base
   set_primary_keys(:yearID, :lgID, :teamID)
 
   belongs_to :managership, :primary_key => [:teamID, :yearID, :lgID], :foreign_key => [:teamID, :yearID, :lgID]
-  belongs_to :manager, :class_name => "Person", :through => :managership
 end
--- app/models/teams.rb
+++
@@ -1,3 +0,0 @@
-class Team < ActiveRecord::Base
-  set_table_name(:Teams)
-end
